8b952c4d179948403c4888ddaf791e2f553dc315
domato153 <dign2030@gmail.com>
2025-07-31 15:05:12 +0900

    Update Dc_UserFilter_Mobile.user.js

--- Dc_UserFilter_Mobile.user.js
+++ Dc_UserFilter_Mobile.user.js
@@ -87,8 +87,8 @@ https://namu.wiki/w/DBAD%20%EB%9D%BC%EC%9D%B4%EC%84%A4%EC%8A%A4
 
 
         /* [수정] 불필요한 PC버전 요소 숨김 */
-        #dc_header, #dc_gnb, .adv_area, .right_content, .dc_all, .dcfoot, .dc_ft, .info_policy, .copyrigh, .ad_bottom_list, .bottom_paging_box + div, .intro_bg, .fixed_write_btn, .bottom_movebox, .zzbang_div, .my_zzal, .my_dccon, .issue_contentbox, #gall_top_recom.concept_wrap,
-        .gall_exposure, .stickyunit, #kakao_search, .banner_box, #ad-layer {
+        #dc_header, #dc_gnb, .adv_area, .right_content, .dc_all, .dcfoot, .dc_ft, .info_policy, .copyrigh, .ad_bottom_list, .bottom_paging_box + div, .intro_bg, .fixed_write_btn, .bottom_movebox, .zzbang_ad ,.zzbang_div,#zzbang_div .my_zzal, .my_dccon, .issue_contentbox, #gall_top_recom.concept_wrap,
+        .gall_exposure, .stickyunit, #kakao_search, .banner_box, #ad-layer,.__dcNewsWidgetTypeB__, .dctrend_ranking, .cm_ad, .con_banner.writing_banbox, [id^="criteo-"]   {
             display: none !important;
         }
 
